deef33d61d39729da51a1b3b79963379c5d78330
eunchan <whdmscks11@gmail.com>
2025-09-10 17:46:39 +0900

    feat: 인증번호 재요청 버튼 추가
    - Shared Preferences 를 통해 인증 요청 횟수 제한
    - 스낵바로 남은 요청 횟수 표시

--- lib/features/auth/presentation/screens/verification_screen.dart
+++ lib/features/auth/presentation/screens/verification_screen.dart
@@ -6,6 +6,7 @@ import 'package:clozii/core/utils/loading_overlay.dart';
 import 'package:clozii/features/auth/presentation/widgets/verification/verification_field.dart';
 import 'package:clozii/features/home/presentation/screens/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class VerificationScreen extends StatefulWidget {
   const VerificationScreen({super.key});
@@ -98,6 +99,78 @@ class _VerificationScreenState extends State<VerificationScreen> {
     });
   }
 
+  /// 인증번호 요청 횟수 제한 관리
+  /// - SharedPreferences를 사용해 앱 종료 후에도 횟수 저장
+  /// - 1분(임시) 동안 최대 5회 제한 (현재 전화번호 구분 로직은 구현 안함)
+  Future<int> _requestCodeCount() async {
+    final requestCooldown = Duration(minutes: 3).inMilliseconds; // 제한 시간 1분
+    final prefs = await SharedPreferences.getInstance(); // 네이티브 저장소 연결
+    final now = DateTime.now().millisecondsSinceEpoch; // 메서드가 호출된 시간 (밀리초)
+
+    int firstRequestTime =
+        prefs.getInt('firstRequestTime') ?? 0; // 제한 횟수 5회 중 첫번째로 요청한 시각
+    int countRemaining = prefs.getInt('countRemaining') ?? 5; // 남은 제한 횟수
+    int diff = now - firstRequestTime; // 메서드가 호출된 시간 - 첫 인증번호 요청 시간
+
+    // 1분이 지났으면 횟수 초기화
+    if (diff > requestCooldown) {
+      await prefs.setInt('firstRequestTime', now); // 제한 시간이 지나면 첫 요청 시간 갱신
+      countRemaining = 5; // 요청 횟수 카운트도 초기화
+    }
+
+    // 핵심 로직‼️
+    // 남은 횟수 감소 후 저장
+    countRemaining--;
+    await prefs.setInt('countRemaining', countRemaining);
+
+    return countRemaining;
+  }
+
+  /// "인증번호 전송" 버튼 클릭 처리
+  void _onSendCodeButtonPressed() async {
+    int count = await _requestCodeCount(); // 요청 제한 횟수 카운트
+    if (count > 0) {
+      _startTimer(); // 타이머 시작
+    }
+
+    // TODO: 실제 인증번호 전송 로직 구현
+    // TODO: count < 0 이면 전송 차단
+    // TODO: 전송된 인증번호를 상태에 저장해 검증 시 사용
+
+    setState(() {
+      // 스낵바 내용 :
+      // - 최초 요청 시 "인증번호 전송됨" 메시지 표시 - "Verfication code sent."
+      Widget content = Text('Verification code sent.');
+
+      // - 제한 횟수가 남아 있는 경우, 남은 요청 가능 횟수 표시 - "N verfication attems remaining."
+      if (count >= 0) {
+        content = Text('$count verification attemps remaining.');
+      }
+
+      // - 제한 횟수를 초과한 경우 "나중에 다시 시도해주세요" 표시 - "Try again later."
+      if (count < 0) {
+        content = Text('Try again later');
+      }
+
+      // 기존 스낵바 제거 - 기존 스낵바가 제거되기 전에 다시 스낵바가 호출될 경우를 대비
+      ScaffoldMessenger.of(context).clearSnackBars();
+
+      // 새 스낵바 표시
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          behavior: SnackBarBehavior.floating,
+          duration: Duration(seconds: 2),
+          shape: ContinuousRectangleBorder(
+            borderRadius: BorderRadiusGeometry.circular(20.0),
+          ),
+
+          // 스낵바 내용
+          content: content,
+        ),
+      );
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -120,6 +193,30 @@ class _VerificationScreenState extends State<VerificationScreen> {
                 seconds: _seconds,
                 onVerified: () {},
                 controller: _controller,
+              ),
+
+              Align(
+                alignment: Alignment.center,
+                child: TextButton(
+                  style: TextButton.styleFrom(
+                    overlayColor: Colors.grey,
+                    minimumSize: Size(0, 0),
+                    padding: EdgeInsets.symmetric(
+                      vertical: 4.0,
+                      horizontal: 12.0,
+                    ),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadiusGeometry.circular(4.0),
+                    ),
+                  ),
+                  onPressed: _onSendCodeButtonPressed,
+                  child: Text(
+                    'Send code again',
+                    style: context.textTheme.labelLarge!.copyWith(
+                      color: context.colors.scrim,
+                    ),
+                  ),
+                ),
               ),
             ],
           ),
